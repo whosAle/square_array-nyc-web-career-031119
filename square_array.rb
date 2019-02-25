3a90daa27282edc509a1e90a7a92b666b0e3ed55
A Belgrave <abelgrave95@gmail.com>
2019-02-25 04:27:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,9 @@
 def square_array(array)
-  # squared = []
-  # array.each do |num|
-  #   squared << num**2
-  # end
-  # squared
+  squared = []
+  array.each do |num|
+    squared << num**2
+  end
+  squared
 
-  array.collect { |num| num**2 }
-  array
+  # array.collect { |num| num**2 }
 end
-
-puts square_array([1,2,3])
